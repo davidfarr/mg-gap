1c3ae5eb947d2154ef972f0860bc136a97845526
David Farr <davidfarr@users.noreply.github.com>
2017-09-20 09:27:53 -0700

    refractor analysis component
    refractored to treat each SNP as an object with relevant properties - will improve output and allow for slimmer or more verbose output per user choice

--- mg-gap/mg-gap/Program.cs
+++ mg-gap/mg-gap/Program.cs
@@ -17,23 +17,36 @@ namespace v1_gap
         public static void Main(string[] args)
         {
             //set up the filepath - in this version it's hard-coded
-            //string vcf_path = "/Users/david/Desktop/Ali.vcf"; //for mac environment only
-            string vcf_path = "N:/app dev/scoville research/program files/dev migration for windows/vcf files/ali.vcf";
+            //string vcf_path = "N:/app dev/scoville research/program files/dev migration for windows/vcf files/ali.vcf";
+            string vcf_path = @"//ENTROPY/All/School/Biology Research/Pilot Program/2_Python program and input files/ali.vcf"; //private environment
 
             //run the vcf parser for SNP window of 1
             Stopwatch methodTime = new Stopwatch();
             Console.WriteLine("Starting B processing at " + DateTime.Now + "...");
             methodTime.Start();
-            List<string> bResult = mg_gap.VcfParser.b_processing(1, vcf_path, 'N'); //window of 1, path, do not get b*
-            methodTime.Stop();
-            Console.WriteLine("B processing time: " + methodTime.Elapsed.ToString());
-            using (StreamWriter bfilenew = File.CreateText("B1_new.txt"))
+
+
+            //new way for R work prep
+            using (StreamWriter write_results = File.CreateText("B1_new.txt"))
             {
-                foreach (var line in bResult)
+                foreach (mg_gap.SNP snp in mg_gap.VCF_Analyzer.snp_list(1, vcf_path, 'N'))
                 {
-                    bfilenew.WriteLine(line);
+                    write_results.WriteLine(snp.Old_identifier + "_" + snp.Basepair + '\t' + snp.B_standard);
                 }
             }
+
+            //old way
+            //List<string> bResult = mg_gap.VcfParser.b_processing(1, vcf_path, 'N'); //window of 1, path, do not get b*
+
+            methodTime.Stop();
+            Console.WriteLine("B processing time: " + methodTime.Elapsed.ToString());
+            //using (StreamWriter bfilenew = File.CreateText("B1_new.txt"))
+            //{
+            //    foreach (var line in bResult)
+            //    {
+            //        bfilenew.WriteLine(line);
+            //    }
+            //}
 
             try
             {
@@ -43,7 +56,8 @@ namespace v1_gap
                 r_stopwatch.Start();
                 REngine engine = REngine.GetInstance();
                 Console.WriteLine("Successfully created R engine instance. Evaluating script...");
-                string rscriptpath = @"N:/app dev/scoville research/program files/github repo/mg-gap/mg-gap/mg-gap/mg-gap/support files/GenWin_script_12_29_2016.R";
+                //string rscriptpath = @"N:/app dev/scoville research/program files/github repo/mg-gap/mg-gap/mg-gap/mg-gap/support files/GenWin_script_12_29_2016.R"; //lab env
+                string rscriptpath = @"C:/Users/David/Documents/GitHub/mg-gap/mg-gap/mg-gap/support files/GenWin_script_12_29_2016.R"; //personal env
                 engine.Evaluate(@"source('" + rscriptpath + "')");
                 r_stopwatch.Stop();
                 Console.WriteLine("R exited successfully at " + DateTime.Now + "\nRun time " + r_stopwatch.Elapsed.ToString());
@@ -55,7 +69,10 @@ namespace v1_gap
 
             //after this then find the median out of the output file and then run the b processing at that window and then b* processing - then move to java program
             //check if we recognize that the file has now been put there
-            string splinepath = "N:/app dev/scoville research/program files/github repo/mg-gap/mg-gap/mg-gap/mg-gap/bin/Debug/splinewindows.txt";
+            //string splinepath = "N:/app dev/scoville research/program files/github repo/mg-gap/mg-gap/mg-gap/mg-gap/bin/Debug/splinewindows.txt";
+            string splinepath = "C:/Users/David/Documents/GitHub/mg-gap/mg-gap/mg-gap/bin/Debug/splinewindows.txt";
+
+
             double median = 0.0;
             if (File.Exists(splinepath) == true)
             {
@@ -112,13 +129,16 @@ namespace v1_gap
             if (median > 0)
             {
                 Console.WriteLine("Re-analyzing for B* based on median window size " + median + " @ " + DateTime.Now);
-                List<string> b_star_Result = mg_gap.VcfParser.b_processing((int)median, vcf_path, 'Y'); //window is median, do get b*
+                //List<string> b_star_Result = mg_gap.VcfParser.b_processing((int)median, vcf_path, 'Y'); //window is median, do get b*
                 Console.WriteLine("Analysis complete at " + DateTime.Now + ", writing B* results file...");
                 using (StreamWriter bsfile = File.CreateText("Bs_" + median + ".txt"))
                 {
-                    foreach (var line in b_star_Result)
+                    bsfile.WriteLine("CHR\tBP\tB\tBs\tP");
+                    foreach (mg_gap.SNP snp in mg_gap.VCF_Analyzer.snp_list(Convert.ToInt32(median), vcf_path, 'Y'))
                     {
-                        bsfile.WriteLine(line);
+                        bsfile.WriteLine(snp.Chromosome + '\t' + snp.Basepair + '\t' +
+                            snp.B_standard + '\t' + snp.B_star + '\t' +
+                            snp.Raw_p + '\t');
                     }
                 }
             }
