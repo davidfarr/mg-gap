dffe4192b9e5547dbc97ce207a9b193f62aa3234
heathem <mckinnonhe@gmail.com>
2019-02-01 19:13:42 -0800

    setup program outline
    setting up file skeleton

---
+++ mg-gap/mg-gap-py/mg-gap/mg-gap/VCF_Analyzer.py
@@ -0,0 +1,1 @@
+class VCF_Analyzer:
---
+++ mg-gap/mg-gap-py/mg-gap/mg-gap/program.py
@@ -0,0 +1,14 @@
+
+# set up the vcf and chisq filepaths
+# TODO enter the correct paths
+vcf_path = "N:/app dev/scoville research/program files/dev migration for windows/vcf files/Ali_w_767.vcf";
+chisq_path = "C:/Users/David/Documents/GitHub/mg-gap/mg-gap/mg-gap/support files/chisq.txt";
+
+# run the vcf parser for SNP window of 1
+
+# TODO there's some code here for timing, then a lot of code that is
+# commented out. Is this important?
+
+# feed the median value back through b processing, then b* 
+# need to hold on to the data for FDR though
+
