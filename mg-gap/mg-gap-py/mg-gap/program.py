861634c44657ccaff8dee92125720da265a897bd
heathem <mckinnonhe@gmail.com>
2019-02-15 09:01:03 -0800

    most of main done
    Just need to add error checking and do tests

--- mg-gap/mg-gap-py/mg-gap/program.py
+++ mg-gap/mg-gap-py/mg-gap/program.py
@@ -21,7 +21,7 @@ start_time = time.time()
 print("Starting B processing at :", start_time)
 
 # TODO question: what is the 'N' in the arguments for VCF_Analyzer?...only takes 3
-# arguments, but with the 'N' is 4
+# arguments, but with the 'N' it has 4 arguments
 write_results = open("B1_new.txt", "w+")
 write_results.write("CHR\tBP\tB\n")
 for snp in VCF_Analyzer.SNP_list(1, vcf_path, 'N', chisq_path):
@@ -60,26 +60,32 @@ Guide to splinewindows format
    * first row is headers, skip
 """
 splinepath = "N:/app dev/scoville research/program files/github repo/mg-gap/mg-gap/mg-gap/bin/Debug/splinewindows.txt";
-# TODO add some error checking code, like try/catch
+# TODO add some error checking code, like try/catch?
 print("GenWin file found, obtaining median window value...")
 windows = []
 with open(splinepath, "r") as sp:
-    sp.readline() # read past the header
+    sp.readline() # read past the header line
     for line in sp:
         if ("CHRcol" not in line):
             cols = []
             cols.append(line.replace("\n", "").split("\t"))
-            windows.append()
-            # NOTE !!! Stopped here
+            windows.append(int(cols[3]))
+if len(windows) == 0:
+    print("No window sizes to calculate!")
+else:
+     windows.sort()
+     middle = len(windows) // 2
+     median = float(windows[middle]) if (len(windows) != 0) else (float(windows[middle]) + float(windows[middle - 1])) / 2.0
+     # TODO question: why are we making median a double/float if we are casting it
+     # back into an integer in the next step
 
 
 # STEP 5 ----------
 #  - feed the median value back through b processing, then b* 
-#  - need to hold on to the data for FDR though
-median = 6
+#  - need to hold on to the data for FDR - make a tab .csv
 if median > 0:
     # TODO need to find equivalent "Convert" method in python see commented code below
-    snpList = VCF_Analyzer.SNP_list(Convert.ToInt32(median), vcf_path, chisq_path)
+    snpList = VCF_Analyzer.SNP_list(int(median), vcf_path, chisq_path)
     print("Re-analyzing for B* based on median window size ", median, " @ ", time.ctime)
 
     # Start the FDR process
@@ -94,32 +100,3 @@ if median > 0:
     # this program. TODO ask about this. Yes we canstop here.
 
     # TODO Something that may be of interest later. Connect to pythosome site to get Gene, description, and RNA SEQ data.
-
-
-# TODO Look up "Convert" method equivalent in python
-"""
-namespace System
-{
-    //
-    // Summary:
-    //     Converts a base data type to another base data type.
-    public static class Convert {
-
-        public static int ToInt32(int value);
-        //
-        // Summary:
-        //     Converts the value of the specified 64-bit signed integer to an equivalent 32-bit
-        //     signed integer.
-        //
-        // Parameters:
-        //   value:
-        //     The 64-bit signed integer to convert.
-        //
-        // Returns:
-        //     A 32-bit signed integer that is equivalent to value.
-        //
-        // Exceptions:
-        //   T:System.OverflowException:
-        //     value is greater than System.Int32.MaxValue or less than System.Int32.MinValue.
-}
-"""
